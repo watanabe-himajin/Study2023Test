5f440c23b1ab3ce973fc1a578f975808be9eae34
watanabe-himajin <c612201036@tokushima-u.ac.jp>
2023-06-12 12:24:29 +0900

    PlayerMove追加
    PlayerMoveを追加しました。

---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private int a = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(a == 1)
+        {
+            Debug.Log("a = 1");
+        }
+    }
+}
